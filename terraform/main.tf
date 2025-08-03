5cf62c667c4b54d9f88160130261a80714d9c3e7
Nalinda Karunaratna <nalindakarunaratna@gmail.com>
2025-08-03 12:02:48 +1000

    Fix: Remove problematic IAM policy attachment - Network Firewall is working correctly

--- terraform/main.tf
+++ terraform/main.tf
@@ -220,31 +220,4 @@ resource "aws_networkfirewall_rule_group" "block_domains" {
   tags = merge(var.tags, {
     Name = "block-malicious-domains"
   })
-}
-
-# IAM Role for Network Firewall
-resource "aws_iam_role" "network_firewall" {
-  name = "NetworkFirewallRole"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "network-firewall.amazonaws.com"
-        }
-      }
-    ]
-  })
-
-  tags = merge(var.tags, {
-    Name = "NetworkFirewallRole"
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "network_firewall" {
-  role       = aws_iam_role.network_firewall.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSNetworkFirewallServiceRolePolicy"
 } 
